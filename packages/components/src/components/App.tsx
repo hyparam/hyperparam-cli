0cb53314ded148d47f4abd70dac9573a2cdf06f2
Sylvain Lesage <severo@rednegra.net>
2024-11-05 13:54:56 +0100

    add intermediate Page component to later handle authentication

--- packages/components/src/components/App.tsx
+++ packages/components/src/components/App.tsx
@@ -1,21 +1,7 @@
-import Cell from './Cell.js'
-import File from './File.js'
-import Folder from './Folder.js'
+import Page from "./Page.tsx";
 
 export default function App() {
-  const search = new URLSearchParams(location.search)
-  const key = search.get('key') || ''
-  if (Array.isArray(key)) throw new Error('key must be a string')
-
-  if (!key || key.endsWith('/')) {
-    // folder view
-    const prefix = key.replace(/\/$/, '')
-    return <Folder prefix={prefix} />
-  } else if (search.has('col') && search.has('row')) {
-    // cell view
-    return <Cell />
-  } else {
-    // file view
-    return <File file={key} />
-  }
+  return (
+    <Page />
+  );
 }
--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -1,9 +1,10 @@
-import App from './App.tsx';
+import App from './Page.tsx';
 import Cell from './Cell.tsx';
 import File from './File.tsx';
 import Folder from './Folder.tsx';
 import Layout from './Layout.tsx';
 import Markdown from './Markdown.tsx';
+import Page from './Page.tsx';
 import Viewer from './Viewer.tsx';
 export * from './viewers/index.ts';
-export { App, Cell, File, Folder, Layout, Markdown, Viewer };
+export { App, Cell, File, Folder, Layout, Markdown, Page, Viewer };
