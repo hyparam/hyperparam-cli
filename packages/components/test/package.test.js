f9bbd737afd5230e85d5531c6a3e7db80773af72
Sylvain Lesage <severo@rednegra.net>
2024-11-06 22:56:49 +0100

    fix npm versions + add test

---
+++ packages/components/test/package.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest'
+import packageJson from '../package.json'
+
+describe('package.json', () => {
+  it('should have the correct name', () => {
+    expect(packageJson.name).toBe('@hyparam/components')
+  })
+  it('should have a valid version', () => {
+    expect(packageJson.version).toMatch(/^\d+\.\d+\.\d+$/)
+  })
+  it('should have MIT license', () => {
+    expect(packageJson.license).toBe('MIT')
+  })
+  it('should have precise dependency versions', () => {
+    const { dependencies, devDependencies } = packageJson
+    const allDependencies = { ...dependencies, ...devDependencies }
+    /// peer dependencies are not checked, because the user might have different versions
+    Object.values(allDependencies).forEach(version => {
+      expect(version).toMatch(/^\d+\.\d+\.\d+$/)
+    })
+  })
+})
