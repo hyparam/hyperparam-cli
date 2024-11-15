8fbccba9e5da0a5d3c3d504bc5108e495cbe5e6c
Sylvain Lesage <severo@rednegra.net>
2024-11-15 23:01:57 +0100

    fix CI

---
+++ apps/hyparquet-demo/test/package.test.js
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest'
+import packageJson from '../package.json'
+
+describe('package.json', () => {
+  it('should have the correct name', () => {
+    expect(packageJson.name).toBe('hyparquet-demo')
+  })
+  it('should have a valid version', () => {
+    expect(packageJson.version).toMatch(/^\d+\.\d+\.\d+$/)
+  })
+  it('should have MIT license', () => {
+    expect(packageJson.license).toBe('MIT')
+  })
+  it('should have precise dependency versions', () => {
+    const { dependencies, devDependencies } = packageJson
+    const allDependencies = { ...dependencies, ...devDependencies }
+    Object.entries(allDependencies).filter(([name]) => !name.startsWith('@hyparam/')).forEach(([, version]) => {
+      expect(version).toMatch(/^\d+\.\d+\.\d+$/)
+    })
+  })
+})
