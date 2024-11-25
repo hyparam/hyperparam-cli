091e3db119d442173983b65e5fc20afa81557035
Sylvain Lesage <severo@rednegra.net>
2024-11-25 14:32:17 +0100

    missing file

---
+++ packages/components/src/components/viewers/Viewer.tsx
@@ -0,0 +1,42 @@
+import { imageTypes } from '../../lib/files.js'
+import { FileKey, UrlKey } from '../../lib/key.js'
+import ImageView from './ImageView.js'
+import MarkdownView from './MarkdownView.js'
+import TableView from './ParquetView.js'
+import TextView from './TextView.js'
+
+interface ViewerProps {
+  parsedKey: FileKey | UrlKey;
+  setError: (error: Error | undefined) => void;
+  setProgress: (progress: number | undefined) => void;
+}
+
+/**
+ * Get a viewer for a file.
+ * Chooses viewer based on content type.
+ */
+export default function Viewer({
+  parsedKey,
+  setError,
+  setProgress,
+}: ViewerProps) {
+  const { fileName } = parsedKey
+  if (fileName.endsWith('.md')) {
+    return <MarkdownView parsedKey={parsedKey} setError={setError} />
+  } else if (fileName.endsWith('.parquet')) {
+    return (
+      <TableView
+        parsedKey={parsedKey}
+        setError={setError}
+        setProgress={setProgress}
+      />
+    )
+  } else if (imageTypes.some((type) => fileName.endsWith(type))) {
+    return <ImageView parsedKey={parsedKey} setError={setError} />
+  }
+
+  // Default to text viewer
+  return (
+    <TextView parsedKey={parsedKey} setError={setError} setProgress={setProgress} />
+  )
+}
