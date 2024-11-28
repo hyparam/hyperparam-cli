4b81a5fa6c9ff88153570fdaa005421c96e1f99b
Sylvain Lesage <severo@rednegra.net>
2024-11-28 13:48:35 +0100

    the port is dynamically assigned

--- packages/components/demo/demo.tsx
+++ packages/components/demo/demo.tsx
@@ -4,7 +4,7 @@ import { App } from '../src/index.ts'
 
 const url = new URL(location.href)
 if (!url.searchParams.has('key')) {
-  location.href = 'http://localhost:5173/?key=https://huggingface.co/datasets/severo/test-parquet/resolve/main/parquet/csv-train-00000-of-00001.parquet'
+  location.href = '/?key=https://huggingface.co/datasets/severo/test-parquet/resolve/main/parquet/csv-train-00000-of-00001.parquet'
 }
 
 const app = document.getElementById('app')
