f9dcc2b6b46374034ffe1017e2c97429fce287c3
Sylvain Lesage <severo@rednegra.net>
2024-12-05 13:42:44 +0100

    remove duplicated code

--- packages/components/src/lib/sources/source.ts
+++ packages/components/src/lib/sources/source.ts
@@ -33,12 +33,3 @@ export interface DirSource extends BaseSource {
 }
 
 export type Source = FileSource | DirSource
-
-export function getFileName(source: string): string {
-  const fileName = source
-    .replace(/\?.*$/, '') // remove query string
-    .split('/')
-    .at(-1)
-  if (!fileName) throw new Error('Cannot extract a filename')
-  return fileName
-}
