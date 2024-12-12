8c0405459c96bed5bcb44d194cbaa57975379451
Sylvain Lesage <severo@rednegra.net>
2024-12-12 19:20:48 +0100

    Two changes: refactor to remove useEffect, and report CSS changes to demo (#129)
    * refactor to remove useEffect
    
    * report CSS changes to the components demo

--- packages/components/src/components/Layout.tsx
+++ packages/components/src/components/Layout.tsx
@@ -21,42 +21,17 @@ interface LayoutProps {
  * @param props.title - page title
  */
 export default function Layout({ children, className, progress, error, title }: LayoutProps) {
-  const [showError, setShowError] = useState(false)
-  const errorMessage = error?.toString()
-  if (error) console.error(error)
-
-  // Reset error visibility when error prop changes
-  useEffect(() => {
-    if (error) {
-      setShowError(true)
-      console.error(error)
-    } else {
-      setShowError(false)
-    }
-  }, [error])
-
   // Update title
   useEffect(() => {
     document.title = title ? `${title} - hyperparam` : 'hyperparam'
   }, [title])
-
   return <main className='main'>
     <Sidebar />
     <div className='content-container'>
       <div className={cn('content', className)}>
         {children}
       </div>
-      <div className={cn('error-bar', showError && 'show-error')}>
-        <div className='error-content'>
-          <span>{errorMessage}</span>
-          <button
-            aria-label='Close error message'
-            className='close-button'
-            onClick={() => { setShowError(false) }}>
-            &times;
-          </button>
-        </div>
-      </div>
+      <ErrorBar error={error}></ErrorBar>
     </div>
     {progress !== undefined && progress < 1 &&
       <div className={'progress-bar'} role='progressbar'>
@@ -74,4 +49,28 @@ function Sidebar() {
 
 export function Spinner({ className }: { className: string }) {
   return <div className={cn('spinner', className)}></div>
+}
+
+function ErrorBar({ error }: { error?: Error }) {
+  const [showError, setShowError] = useState(error !== undefined)
+  const [prevError, setPrevError] = useState(error)
+
+  if (error) console.error(error)
+  /// Reset error visibility when error prop changes
+  if (error !== prevError) {
+    setPrevError(error)
+    setShowError(error !== undefined)
+  }
+
+  return <div className={cn('error-bar', showError && 'show-error')}>
+    <div className='error-content'>
+      <span>{error?.toString()}</span>
+      <button
+        aria-label='Close error message'
+        className='close-button'
+        onClick={() => { setShowError(false) }}>
+      &times;
+      </button>
+    </div>
+  </div>
 }
