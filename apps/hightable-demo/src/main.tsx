1c16a135a19150db4bcc40025293ab801dcd2df5
Sylvain Lesage <severo@rednegra.net>
2025-01-30 14:48:03 +0100

    Fix hightable demo (#152)
    * relative links
    
    * try switching to hash routes, instead of relative paths

--- apps/hightable-demo/src/main.tsx
+++ apps/hightable-demo/src/main.tsx
@@ -1,6 +1,6 @@
 import 'hightable/src/HighTable.css'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Basic from './Basic.js'
 import Controlled from './Controlled.js'
 import Mirror from './Mirror.js'
@@ -9,11 +9,11 @@ import './index.css'
 
 const app = document.getElementById('app')
 if (!app) throw new Error('missing app element')
-ReactDOM.createRoot(app).render(<BrowserRouter>
+ReactDOM.createRoot(app).render(<HashRouter>
   <Routes>
     <Route path="/" element={<Basic />} />
     <Route path="/selection" element={<Selection />} />
     <Route path="/controlled" element={<Controlled />} />
     <Route path="/mirror" element={<Mirror />} />
   </Routes>
-</BrowserRouter>)
+</HashRouter>)
