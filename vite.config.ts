c08cb195ace4f1ace71a8552a55db382701c2cf2
Sylvain Lesage <severo@rednegra.net>
2025-03-04 18:46:40 +0100

    simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,8 @@
-/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'umd'],
-      name: 'HyperparamComponents',
-      fileName: (format) => `index.${format}.min.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react/jsx-runtime': 'jsx',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    sourcemap: true,
-  },
   test: { environment: 'jsdom', globals: true },
 })
