de1751438225cb17acce71623a41ce10d1842b1f
Sylvain Lesage <severo@rednegra.net>
2025-03-06 18:20:07 +0100

    remove comment

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -6,7 +6,6 @@ import { defineConfig } from 'vite'
 
 const __dirname = dirname (fileURLToPath (import.meta.url ))
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
