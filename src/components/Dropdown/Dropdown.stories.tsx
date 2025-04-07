a0813adf6541fc20c137b5afa60f2c290261bfc2
Sylvain Lesage <severo@rednegra.net>
2025-04-07 19:26:48 +0200

    install storybook and add stories for Dropdown component (#207)

---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Dropdown from './Dropdown.js'
+
+const meta: Meta<typeof Dropdown> = {
+  component: Dropdown,
+}
+export default meta
+type Story = StoryObj<typeof Dropdown>;
+export const Default: Story = {
+  args: {
+    children: <>
+      <button>Item 1</button>
+      <button>Item 2</button>
+    </>,
+  },
+}
+
+export const LeftAlign: Story = {
+  args: {
+    label: 'Menu',
+    align: 'left',
+    children: <>
+      <button>Item 1</button>
+      <button>Item 2</button>
+    </>,
+  },
+}
+
+export const RightAlign: Story = {
+  args: {
+    label: 'Very long label for the menu',
+    align: 'right',
+    children: <>
+      <button>Item 1</button>
+      <button>Item 2</button>
+    </>,
+  },
+}
