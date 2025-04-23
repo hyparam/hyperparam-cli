ccb1940fefbb3ccd2472fee32a210a65f82a7c53
Sylvain Lesage <severo@rednegra.net>
2025-04-23 10:10:26 +0200

    Add Versions component in Breadcrumb (#224)
    * Add Versions component in Breadcrumb
    
    Can be used for iceberg versions, or for git branches, for example.
    
    See https://github.com/hyparam/space/issues/3
    
    * Fix styles

---
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ConfigProvider } from '../../hooks/useConfig.js'
+import Breadcrumb from './Breadcrumb.js'
+
+const meta: Meta<typeof Breadcrumb> = {
+  component: Breadcrumb,
+}
+export default meta
+type Story = StoryObj<typeof Breadcrumb>;
+export const Default: Story = {
+  args: {
+    source: {
+      kind: 'file',
+      sourceId: '/part1/part2/file.txt',
+      fileName: 'file.txt',
+      resolveUrl: '/part1/part2/file.txt',
+      sourceParts: [
+        { text: '/', sourceId: '/' },
+        { text: 'part1/', sourceId: '/part1/' },
+        { text: 'part2/', sourceId: '/part1/part2/' },
+      ],
+      versions: {
+        label: 'Branches',
+        versions: [
+          { label: 'master', sourceId: '/part1/part2/file.txt' },
+          { label: 'dev', sourceId: '/part1/part2/file.txt?branch=dev' },
+          { label: 'refs/convert/parquet', sourceId: '/part1/part2/file.txt?branch=refs/convert/parquet' },
+        ],
+      },
+    },
+  },
+  render: (args) => {
+    const config = {
+      routes: {
+        getSourceRouteUrl: ({ sourceId }: { sourceId: string }) => `/files?key=${sourceId}`,
+      },
+      customClass: {
+        versions: 'custom-versions',
+      },
+    }
+    return (
+      <ConfigProvider value={config}>
+        <Breadcrumb {...args}>
+          <input type='text' placeholder="Search..." />
+        </Breadcrumb>
+      </ConfigProvider>
+    )
+  },
+}
--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -2,11 +2,30 @@ import type { ReactNode } from 'react'
 import { useConfig } from '../../hooks/useConfig.js'
 import type { Source } from '../../lib/sources/types.js'
 import { cn } from '../../lib/utils.js'
+import Dropdown from '../Dropdown/Dropdown.js'
 import styles from './Breadcrumb.module.css'
 
 interface BreadcrumbProps {
   source: Source,
   children?: ReactNode
+}
+
+function Versions({ source }: { source: Source }) {
+  const { routes, customClass } = useConfig()
+
+  if (!source.versions) return null
+  const { label, versions } = source.versions
+
+  return <Dropdown label={label} className={cn(styles.versions, customClass?.versions)} align="right">
+    {versions.map(({ label, sourceId }) => {
+      return <a
+        key={sourceId}
+        role="menuitem"
+        href={routes?.getSourceRouteUrl?.({ sourceId })}
+        aria-current={sourceId === source.sourceId ? 'true' : undefined}
+      >{label}</a>
+    })}
+  </Dropdown>
 }
 
 /**
@@ -21,6 +40,7 @@ export default function Breadcrumb({ source, children }: BreadcrumbProps) {
         <a href={routes?.getSourceRouteUrl?.({ sourceId: part.sourceId }) ?? ''} key={depth}>{part.text}</a>
       )}
     </div>
+    {source.versions && <Versions source={source} />}
     {children}
   </nav>
 }
