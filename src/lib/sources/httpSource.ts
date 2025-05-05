5a8b9ba3fea9491a4e29a2858feae20b68da6414
Brian Bleakley <brianbleakley@gmail.com>
2025-05-05 11:46:48 -0700

    undo lint fix

--- src/lib/sources/httpSource.ts
+++ src/lib/sources/httpSource.ts
@@ -52,12 +52,13 @@ function s3listv2(bucket: string, prefix: string) {
     })
 }
 
-function getS3SourceParts(sourceId: string): SourcePart[] {
+function getSourceParts(sourceId: string): SourcePart[] {
   const [protocol, rest] = sourceId.split('://', 2)
   const parts = rest
     ? [`${protocol}://${rest.split('/', 1)[0]}`, ...rest.split('/').slice(1)]
     : sourceId.split('/')
   const sourceParts = [
+    //{ 'text': '/', 'sourceId': '' },
     ...parts.map((part, depth) => {
       const slashSuffix = depth === parts.length - 1 ? '' : '/'
       return {
@@ -77,7 +78,7 @@ export function getHttpSource(sourceId: string, options?: {requestInit?: Request
     return undefined
   }
 
-  const sourceParts = getS3SourceParts(sourceId)
+  const sourceParts = getSourceParts(sourceId)
 
   if (sourceId.endsWith('/')) {
     const url = new URL(sourceId)
@@ -95,12 +96,11 @@ export function getHttpSource(sourceId: string, options?: {requestInit?: Request
       prefix,
       listFiles: () => s3listv2(bucket, prefix).then(items =>
         items
-          .filter(item => Boolean(item.key))
+          .filter(item => item.key !== undefined)
           .map(item => {
-            if (!item.key) return undefined
-            const isDirectory = item.key.endsWith('/')
+            const isDirectory = item.key!.endsWith('/')
             const itemSourceId = `https://${bucket}.s3.amazonaws.com/${item.key}`
-            let name = item.key.split('/').pop() ?? item.key
+            let name = item.key!.split('/').pop() || item.key
             if (name && isDirectory) {
               name = name.replace(prefix, '')
             }
