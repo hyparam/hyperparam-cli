dc1d052fc46910a67d277fdfdf83a40b40be4a97
Sylvain Lesage <severo@rednegra.net>
2025-06-04 23:42:57 +0200

    update storybook with npx storybook@9.0.4 upgrade

--- src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { ConfigProvider } from '../../hooks/useConfig.js'
 import Breadcrumb from './Breadcrumb.js'
 
--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import Dropdown from './Dropdown.js'
 
 const meta: Meta<typeof Dropdown> = {
