81d2403162b55766ee6bf657545f9c2c5dca544f
Sylvain Lesage <severo@rednegra.net>
2025-10-17 14:32:53 +0200

    remove import

--- src/components/SlidePanel/SlidePanel.test.tsx
+++ src/components/SlidePanel/SlidePanel.test.tsx
@@ -1,5 +1,5 @@
 
-import { act, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { ConfigProvider } from '../../hooks/useConfig.js'
